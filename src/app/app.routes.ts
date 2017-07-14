f454d0f6d6e080b294cbd9333cb63f9f8eb377d2
Quinten De Swaef <quintendeswaef@gmail.com>
2017-07-14 15:58:07 +0200

    angular template we'll be using

---
+++ src/app/app.routes.ts
@@ -0,0 +1,36 @@
+/**
+ * Created by wangdi on 26/5/17.
+ */
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './dashboard/home/home.component';
+import { ProfileComponent } from './dashboard/profile/profile.component';
+import { TableComponent } from './dashboard/table/table.component';
+import { NotificationComponent } from './dashboard/notification/notification.component';
+import { SweetAlertComponent } from './dashboard/sweetalert/sweetalert.component';
+import { SettingsComponent } from './dashboard/settings/settings.component';
+import { PriceTableComponent } from './dashboard/component/pricetable/pricetable.component';
+import { PanelsComponent} from './dashboard/component/panels/panels.component';
+
+import { RootComponent } from './dashboard/root/root.component';
+import { LoginComponent } from './page/login/login.component';
+import { LockComponent } from './page/lock/lock.component';
+import { RegisterComponent } from './page/register/register.component';
+
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'lock', component: LockComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'dashboard', component: RootComponent, children: [
+    {path: '', component: HomeComponent},
+    {path: 'profile', component: ProfileComponent},
+    {path: 'table', component: TableComponent},
+    {path: 'notification', component: NotificationComponent},
+    {path: 'alert', component: SweetAlertComponent},
+    {path: 'settings', component: SettingsComponent},
+    {path: 'components/price-table', component: PriceTableComponent},
+    {path: 'components/panels', component: PanelsComponent}
+  ]}
+];
+
+export const routing = RouterModule.forRoot(routes);
+
